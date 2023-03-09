79ae0a44e558bf498a691040b52892a4f56a98d2
bm-197 <bisratmaru111@gmail.com>
2023-03-09 21:05:19 +0000

    numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num < 10:
-        print('0{}'.format(num), end=", ")
+    if num == 99:
+        print('{}'.format(num))
     else:
-        print('{}'.format(num), end=", ")
+        print('{:02}'.format(num), end=", ")
