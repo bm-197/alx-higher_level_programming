b409555f9f6e37216363d939b45f20f335e3b1db
bm-197 <bisratmaru111@gmail.com>
2023-03-09 21:00:35 +0000

    nums

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num < 10:
-        print(f'0{num}', end=", ")
+        print('0{}'.format(num), end=", ")
     else:
-        print(num, end=", ")
+        print('{}'.format(num), end=", ")
