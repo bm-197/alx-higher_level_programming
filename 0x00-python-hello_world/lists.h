ae19d50995e2404a631df523754bba3d6503502c
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-10 21:59:20 +0300

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_ *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
