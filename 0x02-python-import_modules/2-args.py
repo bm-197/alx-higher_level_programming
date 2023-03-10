05f2ebf5eb8623f5c01234bfdb6a6daf99bf8c5a
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-11 00:18:14 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif length == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".foramt(length))
+        print("{} arguments:".format(length))
     for i in range(length):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
