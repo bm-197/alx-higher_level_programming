dc7c2893e1c1f8a39ea0396f14ed22c9718f3cc2
bm-197 <bisratmaru111@gmail.com>
2023-03-10 07:20:38 +0000

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,8 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 != 0:
             print('Fizz', end=" ")
         elif i % 5 == 0 and i % 3 != 0:
-            print('Buzz' , end=" ")
+            print('Buzz', end=" ")
         elif i % 15 == 0:
             print('FizzBuzz', end=" ")
         else:
             print(i, end=" ")
-
