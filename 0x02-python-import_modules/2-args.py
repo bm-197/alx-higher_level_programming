50112dfbcf6a78ee03c3deb62dd362dc4143295b
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-11 00:10:53 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 import sys
 len = len(sys.argv) - 1
 if len == 0:
-    print('0 arguments.')
-if len == 1:
-    print('1 argument:')
+    print("0 arguments.")
+elif len == 1:
+    print("1 argument:")
 else:
      print("{} arguments:".foramt(len(sys.argv) - 1))
-for i in range(cout):
-    print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+for i in range(len):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
