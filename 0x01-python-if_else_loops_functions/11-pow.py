b19a5633d13991d87bc0945b18ff51851e7d9db1
bm-197 <bisratmaru111@gmail.com>
2023-03-10 06:55:19 +0000

    power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,5 +2,8 @@
 def pow(a, b):
     product = 1
     for i in range(b):
-        product*=a
+        if b > 0:
+            product *= a
+        elif b < 0:
+            product *= 1/a
     return product
