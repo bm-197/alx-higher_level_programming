2e5b58d4530be02dffb5ca675dd05a9b4db7453d
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-10 10:03:23 +0300

    Delete 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def pow(a, b):
-    product = 1
-    for i in range(b):
-        if b > 0:
-            product *= a
-        elif b < 0:
-            product *= (1/a)
-    return product
