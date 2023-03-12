c2a84fd286eada05f19420d70be8a8643090ba9b
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-03-12 23:41:00 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    form calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) - 1!= 3:
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     op = ['+', '-', '/', '*']
@@ -13,10 +14,10 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     ops = sys.argv[2]
     if ops == '+':
-        print("{} {} {} = {}".format(a, ops, b, calculator_1.add(a, b))
+        print("{} {} {} = {}".format(a, ops, b, add(a, b))
     elif ops == '-':
-        print("{} {} {} = {}".format(a, ops, b, calculator_1.sub(a, b))
+        print("{} {} {} = {}".format(a, ops, b, sub(a, b))
     elif ops == '/':
-        print("{} {} {} = {}".format(a, ops, b, calculator_1.div(a, b))
+        print("{} {} {} = {}".format(a, ops, b, div(a, b))
     elif ops == '*':
-        print("{} {} {} = {}".format(a, ops, b, calculator_1.sub(a, b))
+        print("{} {} {} = {}".format(a, ops, b, sub(a, b))
