04fb1ee7c28b85a21f388d0e55ce12caa3463483
bm-197 <bisratmaru111@gmail.com>
2023-06-07 02:32:00 -0700

    task 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    """creates a copy of the string, removing the character at the position n"""
+    for i in range(len(str)):
+        if i != n:
+            print('{}'.format(str[i]), end="")
+    print("")
