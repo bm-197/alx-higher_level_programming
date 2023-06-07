f9890f53d53677cf0ec59e4e56df75aa7c24dc9d
bm-197 <bisratmaru111@gmail.com>
2023-06-07 01:29:49 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,8 @@
 
 def uppercase(str):
     """print a string in uppercase followed by a new line"""
-    ascii_code = 0
     for char in str:
-        ascii_code = ord(char)
-        if ascii_code >= 97 and ascii_code < 123:
-            print('{}'.format(chr(ascii_code - 32), end="")
-        else:
-            print('{}'.format(char), end="")
-
+        if ord(char) >= 97 and ord(char) < 123:
+            char = chr(ord(char) - 32)
+        print('{}'.format(char), end="")
+    print("")
