6292459ff70381dbb658296bd1be0e9d31c4968e
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-06-07 12:42:54 +0300

    task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,9 @@
 
 
 def remove_char_at(str, n):
-    """creates a copy of the string, removing the character at the position n"""
+    """
+    creates a copy of the string, removing the character at the position n
+    """
     if n < 0:
         return str
     return (str[:n] + str[n+1:])
