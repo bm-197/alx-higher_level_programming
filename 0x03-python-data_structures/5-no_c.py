aa9e4c5eb4121a295afb990788441577fb28dd1a
bm-197 <bisratmaru111@gmail.com>
2023-06-12 04:01:00 -0700

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """removes all characters c and C from a string."""
+    new_string = ""
+    for string in my_string:
+        if string != 'c' and string != 'C':
+            new_string+=string
+    return new_string
