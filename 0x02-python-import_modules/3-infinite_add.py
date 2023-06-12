917106eb8cc7a7ebbb5d6eb93884ddadbb8a570f
bm-197 <bisratmaru111@gmail.com>
2023-06-12 02:21:00 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 if __name__ == "__main__":
-    import sys
     """result of the addition of all arguments"""
+    import sys
 
     result = 0
     for i in range(1, len(sys.argv)):
