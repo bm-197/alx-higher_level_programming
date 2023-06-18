cf8993bf1d41259edab4385e2ebf79febee7dffd
bm-197 <bisratmaru111@gmail.com>
2023-06-18 03:37:15 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, string) 
+    if not isinstance(roman_string, str) 
     or roman_string == None:
         return None
 
