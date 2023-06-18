61a3ed0f4432917f9f90aff0558a3ed48da27351
bm-197 <bisratmaru111@gmail.com>
2023-06-17 22:31:23 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """replaces all occurrences of 
+    """replaces all occurrences of
     an element by another in a new list"""
 
     new_list = my_list[:]
