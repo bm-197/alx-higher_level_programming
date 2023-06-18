5acebd57709309a29c50994c1e803faae24c9bbc
bm-197 <bisratmaru111@gmail.com>
2023-06-18 10:03:12 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,6 +6,6 @@ def no_c(my_string):
     new_string = ""
     for string in my_string:
         if string != 'c' and string != 'C':
-            new_string+=string
+            new_string += string
 
     return (new_string)
