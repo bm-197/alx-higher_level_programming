4bec68a726b4a010c25daa92e487802d4a28d246
bm-197 <bisratmaru111@gmail.com>
2023-06-18 04:44:46 -0700

    task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -7,4 +7,5 @@ def complex_delete(a_dictionary, value):
         for k, v in a_dictionary.items():
             if v == value:
                 del a_dictionary[k]
+            break
     return a_dictionary
