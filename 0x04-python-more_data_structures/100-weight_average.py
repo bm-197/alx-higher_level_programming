866f25b4ba4adbaa3bb2c63756f9d0ad056286ec
bm-197 <bisratmaru111@gmail.com>
2023-06-18 03:36:00 -0700

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
+    for k, v in a_dictionary.items():
+        if value == v:
+            del a_dictionary[k]
+    return a_dictionary
