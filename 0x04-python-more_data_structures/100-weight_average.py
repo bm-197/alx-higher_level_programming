b34c22b4a8059aa6209595cd67cd56c17c39e46b
bm-197 <bisratmaru111@gmail.com>
2023-06-18 04:13:24 -0700

    100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,4 +4,6 @@ def weight_average(my_list=[]):
     mul_list = [x[0] * x[1] for x in my_list]
     freq_list = [x[1] for x in my_list]
     sum_list = sum(mul_list)
+    if sum(freq_list) == 0:
+        return 0
     return sum_list / sum(freq_list)
