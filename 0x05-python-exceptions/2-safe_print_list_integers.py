46d4d44e248a6ec5ec402fa207a95fd05514b3ed
bm-197 <bisratmaru111@gmail.com>
2023-06-26 06:16:20 -0700

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
+    """
+    print x int in a list.
+
+    Args:
+        my_list (list): The list to be iterated
+        x (int): The number of elements
+
+    Returns:
+        elements printed
+    """
     k = 0
     for i in range(x):
         try:
