0f1bd9d1c3afb911953d6ad4e27acc2b6c947f21
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-07-04 20:28:45 +0300

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
+    @property
     def width(self):
         """Get/set the width of the Rectangle."""
         return self.__width
