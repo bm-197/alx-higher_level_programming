fe54c00800097db68559bd74d7e896fdb34d665b
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-07-11 12:07:24 +0300

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,6 +16,6 @@ def is_same_class(obj, a_class):
         True if obj is a_class; otherwise False
     """
 
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
