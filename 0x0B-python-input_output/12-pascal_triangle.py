6638e088ac72266976009003766560f2f79b4268
bm-197 <bisratmaru111@gmail.com>
2023-07-12 23:05:51 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a function that creates a pascal tri."""
+
+
+def pascal_triangle(n):
+    """Return a pascal triangle.
+    Args:
+        n (int): number of rows for the pascal tri
+    """
+    if n <= 0:
+        return []
+    result = [[1]]
+
+    for i in range(n):
+        tri = result[-1]
+        row = [1]
+        for j in range(i):
+            row.append(tri[j] + tri[j + 1])
+        row.append(1)
+        result.append(row)
+    return result
