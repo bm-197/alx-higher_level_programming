91e7392c543ae5bbf85741b022c7de419097b58e
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-07-13 09:47:18 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,7 @@
 
 class Mylist(list):
     """Implements sorted printing for the built-in list class."""
-
+    
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
