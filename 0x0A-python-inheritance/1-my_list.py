5828ad735ce3a9f8b54bd504075aed2ece868d34
bm-197 <bisratmaru111@gmail.com>
2023-09-11 06:08:29 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class that inherits form class list."""
+
+class MyList(list):
+    """implements sorted printing."""
+
+    def print_sorted(self):
+        """sortes the list.""
+        print(sort(self))
