37a3b6550ee38b28a2d63b4cf9f54cc9c0574956
Bisrat Maru Shibeshi <111707587+bm-197@users.noreply.github.com>
2023-09-13 16:39:33 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,9 +12,9 @@ def pascal_triangle(n):
         return []
     tri = [[1]]
 
-    for i in range(n-2):
+    for i in range(n-1):
         row = [1]
-        for j in range(i + 1):
+        for j in range(i):
             val = tri[i+1][j] + tri[i+1][j+1]
             row.append(val)
         row.append(1)
