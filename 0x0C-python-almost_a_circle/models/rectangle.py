077c49ba4436fe129647b68cfc28134a6248fae3
bm-197 <bisratmaru111@gmail.com>
2023-09-16 13:00:12 -0700

    task 8 and 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,37 @@ class Rectangle(Base):
                                                        self.x, self.y,
                                                        self.width, self.height)
 
+    def update(self, *args, **kwargs):
+        """Assignes an argument to each attribute."""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a = 0:
+                    if arg == None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                else if a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+        if kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k = "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
