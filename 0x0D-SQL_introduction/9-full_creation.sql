982719b8f57571c58dbd1b28ed13eeca21a0a330
bm-197 <bisratmaru111@gmail.com>
2023-10-18 13:05:39 -0700

    task 6 - 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates a table second_table in the database 
+CREATE TABLE `second_table` (`id` INT, `name` VARCHAR(256), `SCORE`INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
