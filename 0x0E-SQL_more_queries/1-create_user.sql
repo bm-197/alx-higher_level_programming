c5993ed6fccab974e3a79048876a974e2a43d466
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-20 21:59:51 +0300

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creates user
 CREATE USER
-    IF NOT EXITSTS 'user_0d_1'@'localhost' 
+    IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
