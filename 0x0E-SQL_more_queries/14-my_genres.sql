b4f43c3467bad87b2ea771ce23588e81288b357c
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-21 09:00:14 +0300

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 USE hbtn_0d_tvshows;
 SELECT t.name
 FROM tv_genres AS t 
-INNER JOIN tv_show_genres AS g ON t.id = g.genre_id
-INNER JOIN tv_shows AS s ON s.id = g.show_id
-WHERE s.title = Dexter
+    INNER JOIN tv_show_genres AS g ON t.id = g.genre_id
+    INNER JOIN tv_shows AS s ON s.id = g.show_id
+    WHERE s.title = Dexter
 ORDER BY t.name;
