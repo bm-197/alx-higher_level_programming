05572bd263217534bc80c013fa58ff2aac10f1e9
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-21 10:30:19 +0300

    task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,17 @@
--- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT DISTINCT s.title
-FROM tv_shows AS s
-    LEFT JOIN tv_show_genres AS g ON t.id = g.genre_id
-    LEFT JOIN tv_genres AS t ON g.genre_id = t.id
-    WHERE s.title (
-        SELECT s.title
-        FROM tv_shows AS s 
-        INNER JOIN tv_show_genres AS g ON s.id = g.show_id
-        INNER JOIN tv_genres AS t ON t.id = g.genre_id 
-        WHERE (t.name = 'Comedy'))
-ORDER BY s.title;
+  FROM tv_shows AS s
+       LEFT JOIN tv_show_genres AS g
+       ON g.show_id = s.id
+
+       LEFT JOIN tv_genres AS t
+       ON t.id = g.genre_id
+       WHERE s.title NOT IN
+             (SELECT s.title
+                FROM tv_shows AS s
+	             INNER JOIN tv_show_genres AS g
+		     ON g.show_id = s.id
+
+		     INNER JOIN tv_genres AS t
+		     ON t.id = g.genre_id
+		     WHERE t.name = "Comedy")
+ ORDER BY s.title;
