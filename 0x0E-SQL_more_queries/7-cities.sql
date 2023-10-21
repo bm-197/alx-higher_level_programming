bbca3a190a381a7b2f65842cdd3e288b38e028a5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-21 08:24:05 +0300

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- Creates the database hbtn_0d_usa and the table cities.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    PRIMARY KEY(id), id NOT NULL AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    PRIMARY KEY(id), id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+    FOREIGN KEY(state_id) REFERENCES states(id));
