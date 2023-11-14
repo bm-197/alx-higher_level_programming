e6b5d83245ffd564298749bb0fbb90736e9c02d5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 16:12:10 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ if (process.argv.length <= 3) {
   } else {
     const args = process.argv.map(Number)
       .slice(2, process.argv.length)
-      .sort((a, b) => b - ba);
+      .sort((a, b) => b - a);
     console.log(args[1]);
   }
