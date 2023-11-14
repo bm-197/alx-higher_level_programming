3ced29fe69955aae36fde835ceba7a381423d7a8
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 15:44:14 +0300

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const lists = ["C is fun", "Pythons is cool", "JavaScript is amzing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const list of lists) {
-    console.log(list);
+for (const language of languages) {
+  console.log(language);
 }
