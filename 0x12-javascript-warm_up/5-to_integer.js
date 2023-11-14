564194b6cf2424fde4e1a673ec8b9ae2ef92a7de
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 15:45:32 +0300

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const val = Math.floor(Number(process.argv[2]));
-console.log(isNaN(val) ? 'Not a number' : 'My number: ${val}');
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
