e6fb47f3c511ba5f98ef194cbc4fa032194f1c46
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 19:21:56 +0300

    advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
 
+const arr = require('./100-data.js').list;
+
+console.log(arr);
+console.log(arr.map((n, i) => n * i));
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js')
+let new_dict = {};
+
+for (let key in dict) {
+    if (new_dict[dict[key]] === undefined) {
+        new_dict[dict[key]] == [key];
+    } else {
+        new_dict[dict[key]].push(key);
+    }
+}
+console.log(new_dict);
