234a579ea6e102a81a9709ef4126e26a6ba467a9
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 15:33:19 +0300

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,10 @@ const num = Number(process.argv[2]);
 
 function factorial (num) {
 
-	if (num > 1)
+	if (num > 0) {
 		return (num * factorial(num - 1));
-	else
-		return 1;
+	}
+	return 1;
 }
+
+console.log(factorial(Number(process.argv[2])));
