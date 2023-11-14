a36be48a45bb9f175734e0eedd6326c01062f3a5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-14 15:42:51 +0300

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-if (!(isNaN(process.argv[2]))) {
+if (isNaN(process.argv[2])) {
+    console.log('Missing number of occurance');
+    
+} else {
     for ( const i = 0; i < process.argv[2]; i++) {
         console.log('C is fun');
     }
-} else {
-    console.log('Missing number of occurance');
 }
