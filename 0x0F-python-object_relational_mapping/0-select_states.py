5dd790227118dbfc93bac57d06d9689ccc51ba02
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-18 17:45:17 +0300

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that list all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM state ORDER BY id")
+    for state in c.fetchall:
+        print(state)
+
