8509578ceb8984e0eeb739dc37dc7d3f68198c1a
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-19 01:21:17 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,3 +7,12 @@ name matches the argument.
 
 import sys
 import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    rows = c.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
