2ac53cba888d3c7f9daafbcc6a5bf725c74859d5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-19 14:17:49 +0300

    task 4: fixing out put error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,9 +10,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT c.id, c.name, s.name\
-        FROM citites as c\
-            INNER JOIN states as s\
-            ON c.state_id = s.id ORDER BY c.id")
-    [print(row) for row in c.fetchall()]
-    
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
