c56c7c7bf4805e60fbb51ebe751170a4ceab186f
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-19 18:58:08 +0300

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,4 +18,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
