990bf6799b591ccd1ab1e6c02358a54a12b56ea7
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-19 13:15:24 +0300

    importing libraries

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Script that takes in arguments and displays all
+values in the states table of hbtn_0e_0_usa where
+name matches the argument. But this time, write one
+that is safe from MySQL injections!
+"""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
