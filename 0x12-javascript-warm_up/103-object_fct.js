4504a5dba39d6686305036c30a4126251d5b0fe5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-23 13:56:42 +0300

    commit advanced tasks

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,16 +2,16 @@
 let myObject = {
     type: 'object',
     value: 12
-  };
-  console.log(myObject);
+};
+console.log(myObject);
   
-  myObject.incr = function () {
+myObject.incr = function () {
     this.value++;
-  };
+};
   
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
