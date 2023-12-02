b5eedfdb8a09b4fb393cca4564a8a37d9f79498d
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-12-02 20:45:24 +0300

    modifying typos

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,12 @@ def find_peak(list_of_integers):
     """a function that finds a peak in a list of unsorted integers."""
     size = len(list_of_integers)
     l = list_of_integers
-    pick = 0
+    peak = 0
     if size == 0:
         return None
     for i in range(size):
         if i != 0 or i != size:
             if l[i] > l[i - 1] and l[i + 1]:
-                pick = l[i]
+                peak = l[i]
 
-    return pick
+    return peak
