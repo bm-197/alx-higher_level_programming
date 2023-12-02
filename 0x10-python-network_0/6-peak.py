aa0ce97f7a62efff5453d394e43d11489ced4c98
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-12-02 16:36:35 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,8 @@ def find_peak(list_of_integers):
     size = len(list_of_integers)
     l = list_of_integers
     pick = 0
+    if size == 0:
+        return None
     for i in range(size):
         if i != 0 or i != size:
             if l[i] > l[i - 1] and l[i + 1]:
